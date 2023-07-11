f77693d07e23501b0366245fb4aae618ddc8d95b
Jason Clare <70923049+mintyModulo@users.noreply.github.com>
2023-07-11 13:09:09 -0500

    Start of Exercise 5.2

---
+++ Chapter_5/Exercise_5.2.c
@@ -0,0 +1,13 @@
+// Write a program that asks fora n integer and then prints all the integers from (and including) that value up to a value larger by 10.
+// (That is, if the input is 5, the output runs from 5 to 15.)
+// Be sure to separate each output value by a space or tab or newline.
+
+#include <stdio.h>
+
+int main(void)
+{
+
+
+
+    return 0;
+}
