b26267dddcb795d4e8457c54609c44d99ef3464e
Jason Clare <70923049+mintyModulo@users.noreply.github.com>
2023-07-11 21:21:00 -0500

    Fairly basic counting program using a while loop. Hardest part was remembering that an extra variable is needed to store the count in.

--- Chapter_5/Exercise_5.2.c
+++ Chapter_5/Exercise_5.2.c
@@ -1,4 +1,4 @@
-// Write a program that asks fora n integer and then prints all the integers from (and including) that value up to a value larger by 10.
+// Write a program that asks for an integer and then prints all the integers from (and including) that value up to a value larger by 10.
 // (That is, if the input is 5, the output runs from 5 to 15.)
 // Be sure to separate each output value by a space or tab or newline.
 
@@ -6,8 +6,21 @@
 
 int main(void)
 {
+    unsigned int count; // declare variable
 
+    // obtain value from user
+    printf("Enter an integer >= 0: ");
+    scanf("%u", &count);
 
+    unsigned int maxCount = count;
+
+    // count from input value to value + 10 and display to output
+    while(count <= maxCount + 10)
+    {   
+        printf("%u\n", count);
+
+        count++;
+    }
 
     return 0;
 }
